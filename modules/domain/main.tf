c9b56468c6234ed9081ab3a6a649777a524db1be
hkavya26 <hkavya26@in.ibm.com>
2021-06-02 12:19:26 +0530

    FIx: Review Comments

--- modules/domain/main.tf
+++ modules/domain/main.tf
@@ -7,23 +7,27 @@ resource "ibm_cis" "cis_instance" {
   location          = "global"
 }
 data "ibm_cis" "cis_instance" {
-  name       = var.service_name
-  depends_on = [ibm_cis.cis_instance]
+  count = var.is_cis_instance_exist == false ? 0 : 1
+  name  = var.service_name
+}
+locals {
+  cis_id    = var.is_cis_instance_exist == false ? ibm_cis.cis_instance[0].id : data.ibm_cis.cis_instance[0].id
+  domain_id = var.is_cis_domain_exist == false ? ibm_cis_domain.cis_domain[0].domain_id : data.ibm_cis_domain.cis_domain[0].domain_id
 }
 resource "ibm_cis_domain" "cis_domain" {
   count  = var.is_cis_domain_exist == false ? 1 : 0
   domain = var.domain
-  cis_id = data.ibm_cis.cis_instance.id
+  cis_id = local.cis_id
 }
 data "ibm_cis_domain" "cis_domain" {
-  domain     = var.domain
-  cis_id     = data.ibm_cis.cis_instance.id
-  depends_on = [ibm_cis_domain.cis_domain]
+  count  = var.is_cis_domain_exist == false ? 0 : 1
+  domain = var.domain
+  cis_id = local.cis_id
 }
 
 resource "ibm_cis_dns_record" "dns_rescords" {
-  cis_id    = data.ibm_cis.cis_instance.id
-  domain_id = data.ibm_cis_domain.cis_domain.domain_id
+  cis_id    = local.cis_id
+  domain_id = local.domain_id
   for_each  = { for record in var.record_set : join("/", [lookup(record, "name", ""), record.type]) => record }
   name      = lookup(each.value, "name", "")
   type      = each.value.type
--- modules/domain/outputs.tf
+++ modules/domain/outputs.tf
@@ -1,25 +1,12 @@
 output "cis_id" {
   description = "CRN of CIS Service Instance"
-  value       = data.ibm_cis.cis_instance.id
+  value       = local.cis_id
 }
 output "domain_id" {
   description = "ID id CIS Domain"
-  value       = data.ibm_cis_domain.cis_domain.domain_id
-}
-output "ibm_cis" {
-  description = "Details of CIS Service Instance"
-  value       = data.ibm_cis.cis_instance
-}
-output "ibm_domain" {
-  description = "Details id CIS Domain"
-  value       = data.ibm_cis_domain.cis_domain
+  value       = local.domain_id
 }
 output "dns_record_ids" {
   description = "Ids CIS DNS Records"
   value       = [for record in ibm_cis_dns_record.dns_rescords : record.id]
-}
-
-output "dns_records" {
-  description = "Details of CIS DNS Records"
-  value       = ibm_cis_dns_record.dns_rescords
 }
