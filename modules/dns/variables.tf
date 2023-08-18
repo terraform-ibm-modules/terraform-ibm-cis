edb8b00193f15e83f7a789e3bd951264bc789c69
Md Anam Raihan <Md.Anam.Raihan@ibm.com>
2023-08-18 12:08:49 +0000

    fix: added dns record type validation

--- modules/dns/variables.tf
+++ modules/dns/variables.tf
@@ -43,4 +43,11 @@ variable "record_set" {
     }))
   }))
   default = []
+
+  validation {
+    condition = alltrue([
+      for record in var.record_set : contains(["A", "AAAA", "CNAME", "NS", "MX", "TXT", "LOC", "SRV", "CAA"], record.type)
+    ])
+    error_message = "The specified DNS type is not a valid selection."
+  }
 }
