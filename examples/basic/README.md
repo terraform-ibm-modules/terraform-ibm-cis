278704fd8520ab75189cd7750937558746b5e449
shikha-mah <shikha.mah@in.ibm.com>
2023-08-18 20:02:37 +0530

    fix: delete basic example

--- examples/basic/README.md
+++
@@ -1,5 +0,0 @@
-# Basic example
-
-An end-to-end basic example that will provision the following:
-- A new resource group if one is not passed in.
-- A new Cloud Internet Services instance.
--- examples/basic/outputs.tf
+++
@@ -1,8 +0,0 @@
-##############################################################################
-# Outputs
-##############################################################################
-
-output "cis_instance_details" {
-  description = "CIS instance details"
-  value       = module.cis_instance
-}
