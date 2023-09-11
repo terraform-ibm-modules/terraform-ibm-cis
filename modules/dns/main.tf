9683bb309bcb9a629a0296dc72eff4e4ccf5b000
Md Anam Raihan <md.anam.raihan17@gmail.com>
2023-09-11 15:00:47 +0530

    fix: added data block validation (#365)
    * The SRV, LOC, CAA type of DNS records needs the data block mandatorily. This release adds a data block validation for those type of DNS records.
    
    ---------
    
    Co-authored-by: Md Anam Raihan <Md.Anam.Raihan@ibm.com>

--- modules/dns/main.tf
+++ modules/dns/main.tf
@@ -2,6 +2,11 @@
 # Add DNS records
 ##############################################################################
 
+locals {
+  # tflint-ignore: terraform_unused_declarations
+  validate_inputs = [for record in var.dns_record_set : (record.type == "SRV" || record.type == "LOC" || record.type == "CAA") ? (record.data != null ? null : tobool("The data{} block in dns_record_set is not defined for the DNS record-  ${record.name}")) : null]
+}
+
 resource "ibm_cis_dns_record" "dns_records" {
   cis_id    = var.cis_instance_id
   domain_id = var.domain_id
