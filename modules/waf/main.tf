8088e6656af49354388267f5de584ca0221dc5e4
Md Anam Raihan <Md.Anam.Raihan@ibm.com>
2023-11-23 13:23:46 +0000

    fix: resolve comments

--- modules/waf/main.tf
+++ modules/waf/main.tf
@@ -6,5 +6,5 @@ resource "ibm_cis_domain_settings" "domain_settings" {
   cis_id          = var.cis_instance_id
   domain_id       = var.domain_id
   waf             = var.waf
-  min_tls_version = "1.2" # The min_tls_version default value was getting modified to 1.1 while terraform apply. Provider issue is open here: https://github.com/IBM-Cloud/terraform-provider-ibm/issues/4937
+  min_tls_version = "1.2" #Temporary fix - The min_tls_version default value (1.2) gets modified to 1.1 while applying domain_settings. This will be reverted back once the provider issue(IBM-Cloud/terraform-provider-ibm#4937) is fixed.
 }
