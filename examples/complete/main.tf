6d91fe767d478898576991b62002820095d46112
Md Anam Raihan <Md.Anam.Raihan@ibm.com>
2024-01-19 03:40:57 +0000

    fix: removed dns-glb-domain-settings

--- examples/complete/main.tf
+++ examples/complete/main.tf
@@ -27,20 +27,20 @@ module "cis_instance" {
 # Add dns records to CIS instance
 ##############################################################################
 
-module "cis_dns_records" {
-  source          = "../../modules/dns"
-  cis_instance_id = module.cis_instance.cis_instance_id
-  domain_id       = module.cis_instance.cis_domain.domain_id
-  dns_record_set = [
-    {
-      type    = "A"
-      name    = "test-example"
-      content = "1.2.3.4"
-      ttl     = 900
-    }
-  ]
-  dns_records_file = "dns_records.txt"
-}
+# module "cis_dns_records" {
+#   source          = "../../modules/dns"
+#   cis_instance_id = module.cis_instance.cis_instance_id
+#   domain_id       = module.cis_instance.cis_domain.domain_id
+#   dns_record_set = [
+#     {
+#       type    = "A"
+#       name    = "test-example"
+#       content = "1.2.3.4"
+#       ttl     = 900
+#     }
+#   ]
+#   dns_records_file = "dns_records.txt"
+# }
 
 ##############################################################################
 # Add global load balancer to CIS instance
@@ -104,11 +104,12 @@ module "cis_dns_records" {
 # resource "null_resource" "next" {
 #   depends_on = [time_sleep.wait_for_cis_instance]
 # }
+
 # cis domain settings
-module "cis_domain_settings" {
-  source = "../../modules/waf"
-  # depends_on      = [module.cis_instance]
-  cis_instance_id = module.cis_instance.cis_instance_id
-  domain_id       = module.cis_instance.cis_domain.domain_id
-  enable_waf      = true
-}
+# module "cis_domain_settings" {
+#   source = "../../modules/waf"
+#   # depends_on      = [module.cis_instance]
+#   cis_instance_id = module.cis_instance.cis_instance_id
+#   domain_id       = module.cis_instance.cis_domain.domain_id
+#   enable_waf      = true
+# }
