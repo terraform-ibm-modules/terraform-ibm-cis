d63a8f456909a150d4ec83f70ec994dc9b4b52f1
Md Anam Raihan <Md.Anam.Raihan@ibm.com>
2024-01-19 06:28:31 +0000

    fix: added depends on

--- examples/complete/main.tf
+++ examples/complete/main.tf
@@ -108,8 +108,8 @@ module "cis_glb" {
 
 # cis domain settings
 module "cis_domain_settings" {
-  source = "../../modules/waf"
-  # depends_on      = [module.cis_instance]
+  source          = "../../modules/waf"
+  depends_on      = [module.cis_instance]
   cis_instance_id = module.cis_instance.cis_instance_id
   domain_id       = module.cis_instance.cis_domain.domain_id
   enable_waf      = true
