ca0bf305ae1d325d705b3613cf610ae33e388cd6
Shikha Maheshwari <shikhamaheshwari@Shikhas-MacBook-Pro.local>
2024-02-16 18:03:44 +0530

    fix: update output file

--- examples/fscloud/outputs.tf
+++ examples/fscloud/outputs.tf
@@ -6,18 +6,3 @@ output "cis_instance" {
   description = "CIS instance details"
   value       = module.cis_instance
 }
-
-# output "cis_dns_records" {
-#   description = "CIS DNS records"
-#   value       = module.cis_dns_records
-# }
-
-# output "cis_glb" {
-#   description = "CIS Global Load Balancer"
-#   value       = module.cis_glb
-# }
-
-# output "cis_domain_settings" {
-#   description = "CIS domain settings"
-#   value       = module.cis_domain_settings
-# }
