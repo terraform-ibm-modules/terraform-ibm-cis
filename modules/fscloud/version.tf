5fbb7f69014fdec6143ab0df1959dffb807882b5
Shikha Maheshwari <shikhamaheshwari@Shikhas-MacBook-Pro.local>
2024-02-21 14:20:29 +0530

    fix: address review comments

--- modules/fscloud/version.tf
+++ modules/fscloud/version.tf
@@ -2,6 +2,11 @@ terraform {
   required_version = ">= 1.3.0, <1.6.0"
 
   required_providers {
+    # tflint-ignore: terraform_unused_required_providers
+    ibm = {
+      source  = "IBM-Cloud/ibm"
+      version = ">= 1.49.0, < 2.0.0"
+    }
     time = {
       source  = "hashicorp/time"
       version = "0.10.0"
