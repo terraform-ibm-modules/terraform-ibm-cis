dff0baf58f2f00829210d35ecd1a5db15d86210a
Vipin Kumar <vipinkumar@Vipins-MacBook-Pro.local>
2025-01-22 13:12:54 +0530

    feat: enable managed rulesets

--- modules/waf/main.tf
+++ modules/waf/main.tf
@@ -2,8 +2,36 @@
 # To enable/disable Web Application Firewall(WAF) for a domain
 ##############################################################################
 
-resource "ibm_cis_domain_settings" "domain_settings" {
-  cis_id    = var.cis_instance_id
-  domain_id = var.domain_id
-  waf       = var.enable_waf ? "on" : "off"
-}
+
+resource "ibm_cis_ruleset_entrypoint_version" "config" {
+    cis_id    = var.cis_instance_id
+    domain_id = var.domain_id
+    phase = "http_request_firewall_managed"
+    rulesets {
+      description = "Entry Point ruleset"
+      rules {
+        action =  "execute"
+        action_parameters  {
+          id = "efb7b8c949ac4650a09736fc376e9aee"
+          }
+        enabled = true 
+        expression = "true"
+      }
+      rules {
+        action =  "execute"
+        action_parameters  {
+          id = "c2e184081120413c86c3ab7e14069605"
+          }
+        enabled = true 
+        expression = "true"
+      }
+      rules {
+        action =  "execute"
+        action_parameters  {
+          id = "4814384a9e5d4991b9815dcfc25d2f1f"
+          }
+        enabled = true 
+        expression = "true"
+      }
+    }
+  }
