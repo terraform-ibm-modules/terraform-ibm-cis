6ad27b177c675ca7b7ac6f0866d0ec8cf059985e
Md Anam Raihan <md.anam.raihan@ibm.com>
2026-02-25 23:32:22 +0530

    fix: update description of resource_tags/tags and access_tags and added extra validation to ensure access tags exist

--- variables.tf
+++ variables.tf
@@ -24,8 +24,12 @@ variable "plan" {
 
 variable "tags" {
   type        = list(string)
-  description = "List of tags to be associated to the CIS instance."
+  description = "Add user resource tags to the Cis instance to organize, track, and manage costs. [Learn more](https://cloud.ibm.com/docs/account?topic=account-tag&interface=ui#tag-types)."
   default     = []
+  validation {
+    condition     = alltrue([for tag in var.tags : can(regex("^[A-Za-z0-9 _\\-.:](1, 128)$", tag))])
+    error_message = "Each resource tag must be 128 characters or less and may contain only A-Z, a-z, 0-9, spaces, underscore (_), hyphen (-), period (.), and colon (:)."
+  }
 }
 
 variable "domain_name" {
