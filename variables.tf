d3e6d7aab88fe0d630faed0b80f514f98a646e48
Md Anam Raihan <md.anam.raihan@ibm.com>
2026-04-07 12:16:09 +0530

    update validation logic

--- variables.tf
+++ variables.tf
@@ -27,7 +27,7 @@ variable "resource_tags" {
   description = "Add user resource tags to the CIS instance to organize, track, and manage costs. [Learn more](https://cloud.ibm.com/docs/account?topic=account-tag&interface=ui#tag-types)."
   default     = []
   validation {
-    condition     = alltrue([for tag in var.resource_tags : can(regex("^[A-Za-z0-9 _\\-.:]{1, 128}$", tag))])
+    condition     = alltrue([for tag in var.resource_tags : can(regex("^[A-Za-z0-9 _\\-.:]{1,128}$", tag))])
     error_message = "Each resource tag must be 128 characters or less and may contain only A-Z, a-z, 0-9, spaces, underscore (_), hyphen (-), period (.), and colon (:)."
   }
 }
