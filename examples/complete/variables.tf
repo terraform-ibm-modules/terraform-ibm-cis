terraform-ibm-modules/terraform-ibm-cis

commit 2d144b40e91b3650af5d5ee06abd126169d8aa8b

fix: Updated default WAF ruleset names to use "Cloudflare" prefix instead of "CIS" prefix to align with official Cloudflare documentation. <br> NOTE: This change only affects default values and does not break existing configurations. Users with custom ruleset configurations are not impacted.(#639)

examples/complete/variables.tf
@@ -35,5 +35,5 @@ variable "glb_name" {
 variable "enable_waf_rulesets" {
   description = "List of rulesets to be enabled for web application firewal(WAF). For more information, refer to the [IBM Cloud Managed Rules Overview](https://cloud.ibm.com/docs/cis?topic=cis-managed-rules-overview)."
   type        = list(string)
-  default     = ["CIS Managed Ruleset", "CIS OWASP Core Ruleset"]
+  default     = ["Cloudflare Managed Ruleset", "Cloudflare OWASP Core Ruleset"]
 }

examples/fscloud/variables.tf
@@ -29,5 +29,5 @@ variable "domain_name" {
 variable "enable_waf_rulesets" {
   description = "List of rulesets to be enabled for web application firewal(WAF). For more information, refer to the [IBM Cloud Managed Rules Overview](https://cloud.ibm.com/docs/cis?topic=cis-managed-rules-overview)."
   type        = list(string)
-  default     = ["CIS Managed Ruleset", "CIS Exposed Credentials Check Ruleset", "CIS OWASP Core Ruleset", "CIS Managed Free Ruleset"]
+  default     = ["Cloudflare Managed Ruleset", "Cloudflare Exposed Credentials Check Ruleset", "Cloudflare OWASP Core Ruleset", "Cloudflare Managed Free Ruleset"]
 }

modules/fscloud/variables.tf
@@ -223,7 +223,7 @@ variable "enable_waf" {
 variable "enable_waf_rulesets" {
   description = "List of rulesets to be enabled for web application firewal(WAF). For more information, refer to the [IBM Cloud Managed Rules Overview](https://cloud.ibm.com/docs/cis?topic=cis-managed-rules-overview)."
   type        = list(string)
-  default     = ["CIS Managed Ruleset", "CIS Exposed Credentials Check Ruleset", "CIS OWASP Core Ruleset", "CIS Managed Free Ruleset"]
+  default     = ["Cloudflare Managed Ruleset", "Cloudflare Exposed Credentials Check Ruleset", "Cloudflare OWASP Core Ruleset", "Cloudflare Managed Free Ruleset"]
 }
 
 variable "use_legacy_waf" {

modules/waf/variables.tf
@@ -28,7 +28,7 @@ variable "use_legacy_waf" {
 variable "enable_waf_rulesets" {
   description = "List of rulesets to be enabled for web application firewal(WAF). For more information, refer to the [IBM Cloud Managed Rules Overview](https://cloud.ibm.com/docs/cis?topic=cis-managed-rules-overview)."
   type        = list(string)
-  default     = ["CIS Managed Ruleset", "CIS Exposed Credentials Check Ruleset", "CIS OWASP Core Ruleset", "CIS Managed Free Ruleset"]
+  default     = ["Cloudflare Managed Ruleset", "Cloudflare Exposed Credentials Check Ruleset", "Cloudflare OWASP Core Ruleset", "Cloudflare Managed Free Ruleset"]
 
   validation {
     condition     = alltrue([for rule in var.enable_waf_rulesets : contains(keys(local.rulesets_map), rule)])

examples/complete/main.tf
@@ -48,6 +48,7 @@ module "cis_dns_records" {
 
 module "cis_glb" {
   source             = "../../modules/glb"
+  depends_on         = [time_sleep.wait_for_cis_instance]
   cis_instance_id    = module.cis_instance.cis_instance_id
   domain_id          = module.cis_instance.cis_domain.domain_id
   glb_name           = join(".", [var.glb_name, var.domain_name])

commit bb25665b218c4361e284c34a0030d3ca87940f45

fix: rename `Cloudflare` ruleset defaults to `CIS` ruleset names. <br> - Reverse the bugs introduced in #639 (#645)

examples/complete/variables.tf
@@ -35,5 +35,5 @@ variable "glb_name" {
 variable "enable_waf_rulesets" {
   description = "List of rulesets to be enabled for web application firewal(WAF). For more information, refer to the [IBM Cloud Managed Rules Overview](https://cloud.ibm.com/docs/cis?topic=cis-managed-rules-overview)."
   type        = list(string)
-  default     = ["Cloudflare Managed Ruleset", "Cloudflare OWASP Core Ruleset"]
+  default     = ["CIS Managed Ruleset", "CIS OWASP Core Ruleset"]
 }

examples/fscloud/variables.tf
@@ -29,5 +29,5 @@ variable "domain_name" {
 variable "enable_waf_rulesets" {
   description = "List of rulesets to be enabled for web application firewal(WAF). For more information, refer to the [IBM Cloud Managed Rules Overview](https://cloud.ibm.com/docs/cis?topic=cis-managed-rules-overview)."
   type        = list(string)
-  default     = ["Cloudflare Managed Ruleset", "Cloudflare Exposed Credentials Check Ruleset", "Cloudflare OWASP Core Ruleset", "Cloudflare Managed Free Ruleset"]
+  default     = ["CIS Managed Ruleset", "CIS Exposed Credentials Check Ruleset", "CIS OWASP Core Ruleset", "CIS Managed Free Ruleset"]
 }

modules/fscloud/variables.tf
@@ -223,7 +223,7 @@ variable "enable_waf" {
 variable "enable_waf_rulesets" {
   description = "List of rulesets to be enabled for web application firewal(WAF). For more information, refer to the [IBM Cloud Managed Rules Overview](https://cloud.ibm.com/docs/cis?topic=cis-managed-rules-overview)."
   type        = list(string)
-  default     = ["Cloudflare Managed Ruleset", "Cloudflare Exposed Credentials Check Ruleset", "Cloudflare OWASP Core Ruleset", "Cloudflare Managed Free Ruleset"]
+  default     = ["CIS Managed Ruleset", "CIS Exposed Credentials Check Ruleset", "CIS OWASP Core Ruleset", "CIS Managed Free Ruleset"]
 }
 
 variable "use_legacy_waf" {

modules/waf/variables.tf
@@ -28,7 +28,7 @@ variable "use_legacy_waf" {
 variable "enable_waf_rulesets" {
   description = "List of rulesets to be enabled for web application firewal(WAF). For more information, refer to the [IBM Cloud Managed Rules Overview](https://cloud.ibm.com/docs/cis?topic=cis-managed-rules-overview)."
   type        = list(string)
-  default     = ["Cloudflare Managed Ruleset", "Cloudflare Exposed Credentials Check Ruleset", "Cloudflare OWASP Core Ruleset", "Cloudflare Managed Free Ruleset"]
+  default     = ["CIS Managed Ruleset", "CIS Exposed Credentials Check Ruleset", "CIS OWASP Core Ruleset", "CIS Managed Free Ruleset"]
 
   validation {
     condition     = alltrue([for rule in var.enable_waf_rulesets : contains(keys(local.rulesets_map), rule)])